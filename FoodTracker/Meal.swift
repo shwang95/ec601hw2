06c99052d2b746e2b0459f661dad2a981ae871dd
shwang95 <shwang95@users.noreply.github.com>
2017-10-08 22:53:49 -0400

    Upload Xcode

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -31,22 +31,18 @@ class Meal: NSObject, NSCoding {
     
     init?(name: String, photo: UIImage?, rating: Int) {
         
-        // The name must not be empty
         guard !name.isEmpty else {
             return nil
         }
 
-        // The rating must be between 0 and 5 inclusively
         guard (rating >= 0) && (rating <= 5) else {
             return nil
         }
         
-        // Initialization should fail if there is no name or if the rating is negative.
         if name.isEmpty || rating < 0  {
             return nil
         }
         
-        // Initialize stored properties.
         self.name = name
         self.photo = photo
         self.rating = rating
@@ -63,18 +59,15 @@ class Meal: NSObject, NSCoding {
     
     required convenience init?(coder aDecoder: NSCoder) {
         
-        // The name is required. If we cannot decode a name string, the initializer should fail.
         guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
             os_log("Unable to decode the name for a Meal object.", log: OSLog.default, type: .debug)
             return nil
         }
         
-        // Because photo is an optional property of Meal, just use conditional cast.
         let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
         
         let rating = aDecoder.decodeInteger(forKey: PropertyKey.rating)
         
-        // Must call designated initializer.
         self.init(name: name, photo: photo, rating: rating)
         
     }
